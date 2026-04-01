a9d72abca42df5898a84d302bd58881514ebdca5
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-04-01 20:23:01 +0530

    App Modules

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@
 # All constants live here. Change once, affects everything.
 # ============================================================
 
+
 PROJECT_NAME    = "SafetyGuard X"
 VERSION         = "1.0.0"
 DESCRIPTION     = (
